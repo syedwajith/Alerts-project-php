28831b9fc3759bc5419ecaf814dab5828d243682
syedwajith <syedwajith768@gmail.com>
2024-02-01 22:51:04 +0530

    fifth commit

--- alert_details.php
+++ alert_details.php
@@ -40,10 +40,16 @@ function get_top_10alerts($connection) {
             create_datetime BETWEEN CONCAT(DATE_SUB(CURDATE(), INTERVAL 1 MONTH), ' 00:00:00') AND CONCAT(CURDATE(), ' 23:59:59')
             ORDER BY create_datetime ASC";
     $top_alert_data = mysqli_query($connection, $top_alert_query);
-
-    print_r($top_alert_data);
-
-    return $top_alert_data;
+    $top_alert_arr = array();
+    while ($row = mysqli_fetch_assoc($top_alert_data)) {
+        $top_alert_arr[] = array($row["customer_name"], $row["alert_type"]);
+        }
+    for ($i = 0; $i < count($top_alert_arr); $i++) {
+        for ($j = 0; $j < count($top_alert_arr[$i]); $j++) {
+            echo $top_alert_arr[$i][$j];
+            echo "<br>";
+        }
+    }
 }
 ?>
 
@@ -58,13 +64,13 @@ function get_top_10alerts($connection) {
     <div class="h2_header">
         <h2>Alert Details</h2>
     </div>
-    <form action="" method="post">
+    <form action="alert_datas.php" method="post">
         <div class="container">
             <div class="mb-3 row">
                 <label class="col-sm-2 col-form-label label-bold">Customer Name</label>
                 <div class="col-sm-10">
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Select Customer Name</option>
+                    <select class="form-select" aria-label="Default select example" name="customer_name" required>
+                        <option selected></option>
                         <?php
                         $customer_names = get_customer_names($connection);
                         while ($row = mysqli_fetch_assoc($customer_names)) {
@@ -77,8 +83,8 @@ function get_top_10alerts($connection) {
             <div class="mb-3 row">
                 <label class="col-sm-2 col-form-label label-bold">Alert Type</label>
                 <div class="col-sm-10">
-                    <select class="form-select" aria-label="Default select example">
-                        <option selected>Select Alert Type</option>
+                    <select class="form-select" aria-label="Default select example" name="alert_type" required>
+                    <option selected></option>
                         <?php
                         $alert_type_names = get_alert_types($connection);
                         while ($row = mysqli_fetch_assoc($alert_type_names)) {
@@ -88,15 +94,15 @@ function get_top_10alerts($connection) {
                     </select>
                 </div>
             </div>
-            <label for="date" class="col-1 col-form-label">Date</label>
-            <div class="col-5">
-                <div class="input-group date" id="datepicker">
-                    <input type="text" class="form-control" id="date"/>
-                    <span class="input-group-append">
-                        <span class="input-group-text bg-light d-block">
-                            <i class="fa fa-calendar"></i>
-                        </span>
-                    </span>
+            <div class="mb-3 row">
+                <label class="col-sm-2 col-form-label label-bold">From Date</label>
+                <div class="col-sm-3">
+                    <input type="date" class="form-control" id="date" name="from_date" required/>
+                </div>
+                <div class="col-sm-2"></div>
+                <label class="col-sm-2 col-form-label label-bold">To Date</label>
+                <div class="col-sm-3">
+                    <input type="date" class="form-control" id="date" name="to_date" required/>
                 </div>
             </div>
             <div class="d-grid gap-2 col-6 mx-auto">
@@ -122,7 +128,7 @@ function get_top_10alerts($connection) {
                 <?php
                 $top_alerts_data = get_top_10alerts($connection);
                 $s_no = 0;
-                while ($row = mysqli_fetch_assoc($top_alerts_data)) {
+                while ($row) {
                     $s_no += 1;
                     echo "<tr>
                             <th scope='row'>$s_no</th>
@@ -140,10 +146,6 @@ function get_top_10alerts($connection) {
     <script>
         $(document).ready( function () {
             $('#myTable').DataTable();
-        });
-
-        $(function(){
-            $('#datepicker').datepicker();
         });
     </script>
     
